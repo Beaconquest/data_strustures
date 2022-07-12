8fcc8b06f364a269293bfb43e5acc50f3eb63149
Promise Mwenga <promise_mwenga@hotmail.com>
2022-07-12 00:42:37 -0300

    refactored test data directly in test functions

--- queue_data_structures/test_custom_queue.py
+++ queue_data_structures/test_custom_queue.py
@@ -19,35 +19,51 @@ class TestCustom_Queue(unittest.TestCase):
     """ UnitTest for the Custom_Queue class from the custom_queue module. """
 
     def test_is_empty(self):
+        cq = Custom_Queue(2)
         self.assertTrue(cq.is_empty())
 
     def test_is_full(self):
-        self.assertTrue(cq1.is_full())
+        cq = Custom_Queue(2)
+        cq.items.append('test')
+        cq.items.append('test2')
+        self.assertTrue(cq.is_full())
 
     def test_enqueue_exception(self):
+        cq = Custom_Queue(1)
+        cq.items.append('test')
         with self.assertRaises(Exception):
-            cq1.enqueue('test2')
+            cq.enqueue('test2')
 
     def test_enqueue(self):
-        cq2.enqueue('test_enqueue')
+        cq = Custom_Queue(2)
+        cq.enqueue('test_enqueue')
         self.assertEqual(['test_enqueue'], ['test_enqueue'])
 
     def test_dequeue_exception(self):
+        cq = Custom_Queue(2)
         with self.assertRaises(Exception):
-            cq2.dequeue()
+            cq.dequeue()
 
     def test_dequeue(self):
-        self.assertEqual(cq3.dequeue(), 'dequeue_test')
+        cq = Custom_Queue(2)
+        cq.items.append('test')
+        self.assertEqual(cq.dequeue(), 'test')
 
     def test_qsize(self):
-        self.assertEqual(cq1.qsize(), 1)
+        cq = Custom_Queue(2)
+        cq.items.append('test')
+        self.assertEqual(cq.qsize(), 1)
 
     def test_peek_exception(self):
+        cq = Custom_Queue(2)
         with self.assertRaises(Exception):
             cq.peek()
 
     def test_peek(self):
-        self.assertEqual(cq3.peek(), 'peek_test')
+        cq = Custom_Queue(2)
+        cq.items.append('test1')
+        cq.items.append('test2')
+        self.assertEqual(cq.peek(), 'test1')
 
 if __name__ == '__main__':
     unittest.main()
